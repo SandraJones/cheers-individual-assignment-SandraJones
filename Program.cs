0f0e68d46f8b5d9997068fcbec8b5913227d2919
Sandy Jones <sandynashv@gmail.com>
2016-07-12 17:19:43 -0500

    2 solution classwork

--- Program.cs
+++ Program.cs
@@ -9,27 +9,60 @@ namespace Cheers
     {
         public static void Main(string[] args)
         {
-            //variables section
+            // Variables section
             String prompt = "> ";
             String cheer_prefix = "Give me a";
-            
+            String requires_an = "aeiolnfshrmx";
+
+
             Console.WriteLine("What's your name?");
             Console.Write(prompt);
-            String user_name = Console.ReadLine();//read from terminal save to a variable
-            //input has to be a string above
-            Console.WriteLine(user_name);
-            
-            for(int i =0; i < user_name.Length; i++){
-                Console.WriteLine(cheer_prefix +  " .. "+ user_name[i]);
+            String user_name = Console.ReadLine().ToLower(); // Read from terminal save to a variable
 
+
+            // First Refactoring removing the 'else'
+            /* 
+            for(int i = 0; i < user_name.Length; i++) {
+                String cheer_prefix = "Give me a"; // Resets the cheer_prefix variable every iteration through for loop
+                if (requires_an.IndexOf(user_name[i]) > -1) {
+                    cheer_prefix += "n";
+                }
+                Console.WriteLine(cheer_prefix + ".. " + user_name[i]);
             }
-            
-            //user_name.ToUpper() is turning user's name to uppercase
-            //ToUpper() is functionality specific to strings
-            //ToUpper() accepts no arguments
+            */
 
-            Console.WriteLine(user_name.ToUpper() + " is..GRAND!");
 
+
+            // Last Refactoring using Contains instead of 'IndexOf'
+
+            for(int i = 0; i < user_name.Length; i++) {
+                if (requires_an.Contains(user_name[i])) { // Performs implicit casting from Char to String
+                    Console.WriteLine(cheer_prefix + "n" + ".. " + user_name[i]);
+                } else {
+                    Console.WriteLine(cheer_prefix + ".. " + user_name[i]);
+                }
+            }
+
+
+
+            // Original Solution, using 'IndexOf'
+            /* 
+            for(int i = 0; i < user_name.Length; i++) {
+                if (requires_an.IndexOf(user_name[i]) > -1) {
+                    // Then it's in the requires_an array!!!!
+                    Console.WriteLine(cheer_prefix + "n" + ".. " + user_name[i]);
+                } else {
+                    // Does not require an "an"
+                    Console.WriteLine(cheer_prefix + ".. " + user_name[i]);
+                }
+            }
+            */
+
+            // user_name.ToUpper() is turning the the user's name to uppercase.
+            // ToUpper() is functionality specific to Strings.
+            // ToUpper() accepts no arguments
+
+            Console.WriteLine(user_name.ToUpper() + " is.. GRAND!"); //
         }
     }
 }
