a681a6cce95e3afb8d60cc52c9c396387007d789
Sandy Jones <sandynashv@gmail.com>
2016-07-11 19:30:49 -0500

    toUpperCase

--- Program.cs
+++ Program.cs
@@ -11,9 +11,13 @@ namespace Cheers
         {
             Console.WriteLine("What's your name?");
             Console.Write("> ");
-            String user_name = Console.ReadLine();
+            String user_name = Console.ReadLine();//read from terminal save to a variable
             //input has to be a string above
-            
+            Console.WriteLine(user_name);
+            //user_name.ToUpper() is turning user's name to uppercase
+            //ToUpper() is functionality specific to strings
+            //ToUpper() accepts no arguments
+            Console.WriteLine(user_name.ToUpper() + " is..GRAND!");
         }
     }
 }
